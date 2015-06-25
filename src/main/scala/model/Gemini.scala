83a9df31c12dcb725f0e9d17609572ebd4d7b7f7
Jerry Yan <jiarui.yan@accenture.com>
2015-06-25 14:10:44 -0400

    add domain model for gemini raw and parsed

--- src/main/scala/model/Gemini.scala
+++ src/main/scala/model/Gemini.scala
@@ -1,13 +1,231 @@
 package model
 
+import kafka.serializer.Decoder
+import kafka.utils.VerifiableProperties
+import org.joda.time.DateTime
+import org.joda.time.format._
+
 /**
  * Created by jiarui.yan on 5/31/15.
  */
-class GeminiRaw {
 
+class GeminiRaw(args: Array[String]) {
+
+  val Array(c_info, dynamic_ip, c_ip, date_string, cs_method, cs_referrer, sc_status, sc_stream_bytes, sc_bytes, cs_uri, cs_user_agent, x_duration, s_cache_status, event_type, unix_time, http_range, cs_cookie, download_speed) = args
 }
 
-class GeminiParsed {
+class GeminiRawDecoder(props: VerifiableProperties = null) extends Decoder[GeminiRaw] {
+  val encoding =
+    if(props == null)
+      "UTF8"
+    else
+      props.getString("serializer.encoding", "UTF8")
 
+  def fromBytes(bytes: Array[Byte]): GeminiRaw = {
+    val logLine = new String(bytes, encoding)
+    new GeminiRaw(logLine.split('\t'))
+  }
 }
 
+class GeminiParsed(rawLog: GeminiRaw) {
+  val raw = rawLog
+
+  def parseRawValue(s: String) = Option(s).filter(x => !x.equals("-") && !x.equals("\"-\""))
+
+  def parseIntRawValue(f: String => Int)(s: String) = {
+    parseRawValue(s) match {
+      case Some(x) => Some(f(x))
+      case _ => None
+    }
+  }
+
+  def parseLongRawValue(f: String => Long)(s: String) = {
+    parseRawValue(s) match {
+      case Some(x) => Some(f(x))
+      case _ => None
+    }
+  }
+
+  def parseDatePartRawValue(f: (String, String) => String)(s: String, part: String) = {
+    parseRawValue(s) match {
+      case Some(x) => Some(f(x, part))
+      case _ => None
+    }
+  }
+
+  def datePartUtil(outFormat:String, dateStr: String) = {
+    val intFmt = DateTimeFormat.forPattern("dd/MMM/yyyy:HH:mm:ss Z")
+    val inDateTime = DateTime.parse(dateStr, intFmt)
+    DateTimeFormat.forPattern(outFormat).withZoneUTC().print(inDateTime)
+  }
+
+  def clientIp = parseRawValue(raw.c_ip)
+
+  def dateTime = parseRawValue(raw.date_string)
+
+  def eventType = {
+    def isTest(s: String) = s.toLowerCase.contains("hls-lin-test")
+
+    def isGenres(s: String) = s.toLowerCase.contains("genres")
+
+    def isCrossDomain(s: String) = s.toLowerCase.contains("crossdomain.xml")
+
+    def isClientAccessPolicy(s: String) = s.toLowerCase.contains("clientaccesspolicy.xml")
+
+    def isBadReq(s: String) = s.toLowerCase.contains("BAD_REQUEST")
+
+    def isVideoChunk(s: String) = s.toLowerCase.contains(".ts") ||
+      s.toLowerCase.contains("Fragments(video".toLowerCase) ||
+      s.toLowerCase.contains("FragmentInfo(video".toLowerCase)
+
+    def isAudioChunk(s: String) = s.toLowerCase.contains("Fragments(audio".toLowerCase) ||
+      s.toLowerCase.contains("FragmentInfo(audio".toLowerCase)
+
+    def isEngAudioChunk(s: String) = s.toLowerCase.contains("Fragments(301_eng".toLowerCase) ||
+      s.toLowerCase.contains("FragmentInfo(301_eng".toLowerCase) ||
+      s.toLowerCase.contains("Fragments(302_eng".toLowerCase) ||
+      s.toLowerCase.contains("FragmentInfo(302_eng".toLowerCase)
+
+    def isTextChunk(s: String) = s.toLowerCase.contains("Fragments(textstream".toLowerCase) ||
+      s.toLowerCase.contains("FragmentInfo(textstream".toLowerCase)
+
+    def isManifestChunk(s: String) = s.toLowerCase.contains("isml/Manifest".toLowerCase) ||
+      s.toLowerCase.contains("ism/Manifest".toLowerCase)
+
+    def isManifest(s: String) = s.toLowerCase.contains(".m3u8")
+
+    def isKey(s: String) = s.toLowerCase.contains(".key")
+
+    def chunkName(s: String) = s match {
+      case csm if isAudioChunk(csm) => "AUDIO_CHUNK"
+      case csm if isVideoChunk(csm) => "VIDEO_CHUNK"
+      case csm if isManifestChunk(csm) => "MANIFEST_CHUNK"
+      case csm if isTextChunk(csm) => "TEXT_CHUNK"
+    }
+
+    val csMethod = raw.cs_method
+    if (csMethod.equals("-") || csMethod.equals("\"-\"")) {
+      None
+    } else {
+      Option(
+        raw.cs_method match {
+          case csm if (isTest(csm)) => "TEST_REQUEST"
+          case csm if (isGenres(csm)) => "GENRE_SERVICE_REQUEST"
+          case csm if (isCrossDomain(csm)) => "CROSS_DOMAIN_XML"
+          case csm if (isClientAccessPolicy(csm)) => "CLIENT_ACCESS_POLICY"
+          case csm if (isBadReq(csm)) => "BAD_REQUEST"
+          case csm if (isEngAudioChunk(csm)) => (chunkType, streamType) match {
+            case (Some("Linear"), Some("SS")) => "LINEAR_SS_AUDIO_CHUNK_ENG"
+            case _ => "CDN_UNKNOWN"
+          }
+          case csm if isManifest(csm) => (chunkType, streamType) match {
+            case (Some(ct), Some("HLS")) => ct.toUpperCase + "_HLS_MANIFEST"
+            case _ => "UNKNOWN_HLS_MANIFEST"
+          }
+          case csm if isKey(csm) => (chunkType, streamType) match {
+            case (Some(ct), Some("HLS")) => ct.toUpperCase + "_HLS_KEY"
+            case _ => "UNKNOWN_HLS_KEY"
+          }
+          case _ => (chunkType, streamType) match {
+            case (Some(ct), Some(st)) => Array(ct.toUpperCase, st.toUpperCase, chunkName(raw.cs_method)).mkString("_")
+            case (None, Some(st)) => Array("UNKNOWN", st.toUpperCase, chunkName(raw.cs_method)).mkString("_")
+            case _ => "CDN_UNKNOWN"
+          }
+        }
+      )
+    }
+
+
+  }
+
+  def httpStatusCode = parseIntRawValue(x => x.toInt)(raw.s_cache_status)
+
+  def userAgent = parseRawValue(raw.cs_user_agent)
+
+  def contentSize = parseLongRawValue(x => x.toLong)(raw.sc_stream_bytes)
+
+  def targetServer = parseRawValue(raw.event_type)
+
+  def year = parseDatePartRawValue(datePartUtil)(raw.date_string.substring(1, raw.date_string.length-1), "yyyy")
+
+  def month = parseDatePartRawValue(datePartUtil)(raw.date_string.substring(1, raw.date_string.length-1), "MM")
+
+  def day = parseDatePartRawValue(datePartUtil)(raw.date_string.substring(1, raw.date_string.length-1), "dd")
+
+  def hour = parseDatePartRawValue(datePartUtil)(raw.date_string.substring(1, raw.date_string.length-1), "HH")
+
+  def minute = parseDatePartRawValue(datePartUtil)(raw.date_string.substring(1, raw.date_string.length-1), "mm")
+
+  def second = parseDatePartRawValue(datePartUtil)(raw.date_string.substring(1, raw.date_string.length-1), "ss")
+
+  def utc = parseLongRawValue(x => x.toLong)(raw.unix_time)
+
+  def mappedUserAgent = ???
+
+  def cacheDeliveryType = parseRawValue(raw.s_cache_status)
+
+  def contentType = ???
+
+  def videoAssetTime = ???
+
+  def bitRate = ???
+
+  def abrLevel = ???
+
+  def chunkSeq = ???
+
+  def sDns = parseRawValue(raw.c_info) match {
+    case Some(s) => s.substring(s.indexOf("nginx-access: ") + "nginx-access: ".length)
+    case _ => None
+  }
+
+  def sIp = ???
+
+  def csMethod = parseRawValue(raw.cs_method)
+
+  def csVersion = ???
+
+  def csReferrer = parseRawValue(raw.cs_referrer)
+
+  def scBytes = parseLongRawValue(x=>x.toLong)(raw.sc_bytes)
+
+  def csUri = parseRawValue(raw.cs_uri)
+
+  def xDuration = parseIntRawValue(x=>x.toInt)(raw.x_duration)
+
+  def dynamicLoadIp = parseRawValue(raw.dynamic_ip)
+
+  def streamType = raw.cs_method match {
+    case st if st.indexOf("/ss/") != -1 => Some("SS")
+    case st if st.indexOf("/hls/") != -1 => Some("HLS")
+    case _ => None
+  }
+
+  def chunkType = raw.event_type match {
+    case ct if ct.contains("linear") => Some("Linear")
+    case ct if ct.contains("video") != -1 => Some("Vod")
+    case _ => None
+  }
+
+  def downloadTime = parseIntRawValue((x => x.toInt))(raw.download_speed)
+
+  def sessionId = ???
+  def logSessionId = ???
+  def userGUID = ???
+  def sVxRate = ???
+  def sVxRateStatus = ???
+  def xVxSerial = ???
+  def rsBytes = ???
+  def xProtohash = ???
+  def csVxToken = ???
+  def cPort = ???
+  def cVxZone  = ???
+  def csCookie  = ???
+  def scVxDownloadRate = ???
+  def requestId = ???
+  def drmClassification = ???
+  def cdnLocation = ???
+  def serverName = ???
+
+
+}
--- src/main/scala/streaming/KafkaApp1.scala
+++ src/main/scala/streaming/KafkaApp1.scala
@@ -1,6 +1,7 @@
 package streaming
 
 import kafka.serializer.StringDecoder
+import model.{GeminiParsed, GeminiRawDecoder, GeminiRaw}
 import org.apache.spark.SparkConf
 import org.apache.spark.streaming.kafka.KafkaUtils
 import org.apache.spark.streaming.{Seconds, StreamingContext}
@@ -12,35 +13,57 @@ object KafkaApp1 {
 
   def main(args: Array[String]) {
 
-    val conf = new SparkConf().setMaster("local[2]").setAppName("Kafka Integration Test")
-    val ssc = new StreamingContext(conf, Seconds(10))
+    val Array(appName, brokerList, topic, rddInterval) = args
+
+    // sbt "run-main streaming.KafkaApp1 local[2] Kafka-Integration dnvrco01-os-coe0002.conops.timewarnercable.com:9092 gemini_v01 5"
+
+
+    //val conf = new SparkConf().setMaster("local[2]").setAppName("Kafka Integration Test")
+    val conf = new SparkConf().setAppName(appName)
+    val ssc = new StreamingContext(conf, Seconds(rddInterval.toLong))
 
     // Connecting to Kafka queue for Gemini 71.74.186.212:9092
-    val kafkaParams = Map("metadata.broker.list" -> "localhost:9002")
+    // "dnvrco01-os-coe0002.conops.timewarnercable.com:9092"
+    val kafkaParams = Map("metadata.broker.list" -> brokerList)
 
-    val topics = Set("gemini_v03")
+    // "gemini_v03"
+    val topics = Set(topic)
 
-    val events = KafkaUtils.createDirectStream[String, String, StringDecoder, StringDecoder](ssc, kafkaParams, topics).map(_._2)
+    val events = KafkaUtils.createDirectStream[String, GeminiRaw, StringDecoder, GeminiRawDecoder](ssc, kafkaParams, topics).map(_._2)
 
+    val parsedEvents = events map (gr => new GeminiParsed(gr))
 
-    val logLineByEdgeNodes = events.map(l => {
+    val logLineByEdgeNodesParsed = parsedEvents.map(l => (l.sDns, 1)).reduceByKey((x,y) => x + y)
+    logLineByEdgeNodesParsed.print(50)
 
-      val fields = l.split('\t')
-      val serverName = fields(0).substring(fields(0).indexOf("nginx-access: ") + "nginx-access: ".length)
-      (serverName, 1)
-    }).reduceByKey((x,y) => x + y)
-
-
-    logLineByEdgeNodes.print(50)
+    val eventTypeCount = parsedEvents map (l => (l.eventType map _, 1)) reduceByKey((x,y) => x+y)
+    eventTypeCount.print(50)
 
 
-    // Total content size of HIT and MISS
-    events.filter(l => l.split('\t').length >= 17).map(l => {
-      val fields = l.split('\t')
-      val hit = fields(7).toLong
-      val missed = if ("MISS".equals(fields(12))) hit else 0L
-      (hit, missed)
-    }).reduce( {case ((x,y),(a,b)) => (x+y, a+b)} ).print()
+//    val splittedRdd = events.map(rdd => rdd.split('\t'))
+//
+//    val fullPartialLogCount = splittedRdd.map(lArr => (lArr.length, 1)).reduceByKey((x, y) => x + y)
+//
+//    fullPartialLogCount.print(50)
+//
+//    val partialLogSample = splittedRdd.filter(lArr => lArr.length < 15).map(lArr => lArr.mkString("|"))
+//
+//    partialLogSample.print(5)
+//
+//    val fullLogSample = splittedRdd.filter(lArr => lArr.length > 15).map(lArr => lArr.mkString("|"))
+//
+//    fullLogSample.print(5)
+
+
+//
+//
+//    // Total content size of HIT and MISS
+//    events.filter(l => l.split('\t').length >= 17).map(l => {
+//      val fields = l.split('\t')
+//      val hit = fields(7).toLong
+//      val missed = if ("MISS".equals(fields(12))) hit else 0L
+//      (hit, missed)
+//    }).reduce( {case ((x,y),(a,b)) => (x+y, a+b)} ).print()
 
     ssc.start()
     ssc.awaitTermination()
